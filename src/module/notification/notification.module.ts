0de23f45ce38fcea70f3c4164d683d33c464c8cc
rmagur1203 <rmagur1203@naver.com>
2025-05-17 12:16:34 +0900

    refactor: simplify NotificationModule by removing interface provider and directly exporting NotificationService

--- src/module/notification/notification.module.ts
+++ src/module/notification/notification.module.ts
@@ -1,17 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { NotificationService } from './notification.service';
-import { INotificationService } from './interface/notification.service.interface';
 
 @Module({
   imports: [ConfigModule],
-  providers: [
-    {
-      provide: INotificationService,
-      useClass: NotificationService,
-    },
-  ],
-  exports: [INotificationService],
+  providers: [NotificationService],
+  exports: [NotificationService],
 })
 export class NotificationModule {}
 
