69f050df821af8a2995538f15b08c4a8df0bdbd4
kewool <kkttyy324@gmail.com>
2025-05-19 13:01:24 +0900

    fixed: remove admin log module because of error

--- src/api/v2/admin/admin-controller.module.ts
+++ src/api/v2/admin/admin-controller.module.ts
@@ -7,7 +7,7 @@ import { AdminDepositControllerModule } from './deposit/admin-deposit-controller
 import { AdminOrderControllerModule } from './order/admin-order-controller.module';
 import { AdminBroadcastControllerModule } from './broadcast/admin-broadcast-controller.module';
 import { AdminFeeControllerModule } from './settings/admin-fee-controller.module';
-import { AdminLogControllerModule } from './logs/admin-log-controller.module';
+// import { AdminLogControllerModule } from './logs/admin-log-controller.module';
 
 @Module({
   imports: [
@@ -19,7 +19,6 @@ import { AdminLogControllerModule } from './logs/admin-log-controller.module';
     AdminOrderControllerModule,
     AdminBroadcastControllerModule,
     AdminFeeControllerModule,
-    AdminLogControllerModule,
   ],
 })
 export class AdminControllerModule {}
